a57492b6d7877bfae0a08a5df4dc2f8120e576a7
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2023-01-29 22:32:51 +0300

    randomized dice

--- DiSpace/ViewController.swift
+++ DiSpace/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceOne: UIImageView!
+   
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    let ballArray = [ #imageLiteral(resourceName: "1") , #imageLiteral(resourceName: "2") , #imageLiteral(resourceName: "3") , #imageLiteral(resourceName: "4") , #imageLiteral(resourceName: "5")  , #imageLiteral(resourceName: "6") ]
+  
+    @IBAction func rollDicePress(_ sender: UIButton) {
+        diceOne.image = ballArray.randomElement()
+        diceTwo.image = ballArray.randomElement()
+        
     }
-
+    
 
 }
 
