e8dc3704e3cea6544ff65e164f1bb45fc3f89bfb
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2023-01-31 14:04:53 +0300

    auto layout done.

--- DiSpace/ViewController.swift
+++ DiSpace/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceOne: UIImageView!
    
+    @IBOutlet weak var diceOne: UIImageView!
+    
     @IBOutlet weak var diceTwo: UIImageView!
     
     let ballArray = [ #imageLiteral(resourceName: "1") , #imageLiteral(resourceName: "2") , #imageLiteral(resourceName: "3") , #imageLiteral(resourceName: "4") , #imageLiteral(resourceName: "5")  , #imageLiteral(resourceName: "6") ]
   
-    @IBAction func rollDicePress(_ sender: UIButton) {
+    
+    @IBAction func rollTheDice(_ sender: UIButton) {
         diceOne.image = ballArray.randomElement()
         diceTwo.image = ballArray.randomElement()
         
